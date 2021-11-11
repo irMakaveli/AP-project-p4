411165178150925ee3a06c82fd96fe73bfee8d21
irMakaveli <hosein.acc2@gmail.com>
2021-11-11 09:17:05 -0500

    classes added

---
+++ ap-project-p1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+class Money
+{
+    int money;
+    public:
+    Money(int);
+    void setmoney(int );
+    int getmoney();
+    int getmoney(int , int);
+};
+Money::Money(int money)
+{
+    setmoney(money);
+}
+void Money::setmoney(int value)
+{
+    if(value < 0 && value + money < 0)
+    {
+        cout<<"wrong value"<<endl;
+        return;
+    } 
+    else
+    {
+        this->money += value;
+    }
+}
+int Money::getmoney()
+{
+    return this->money;
+}
+int Money::getmoney(int count , int price)
+{
+    if((count*price)>money)
+    {
+        cout<<"you have not enough money"<<endl;
+        exit;//exception
+    }
+    if((count*price)<0)
+    {
+        cout<<"wrong values"<<endl;
+        exit;//exception
+    }
+
+    this->money -=(count*price);
+    return count*price;
+}
+class feed
+{
+    int feeds;
+    public :
+    feed(int);
+    void setFeed(int);
+    int getFeed();
+};
+class Hen
+{
+    int HenCount;
+    int egg=0;
+    public:
+    Hen(int , int);//count , price
+    void setHen(int);
+    int getHen();
+    void setEgg(int);
+    int getEgg();
+};
+class Ship
+{
+    int ShipCount=0;
+    int shipMilk=0;
+    public:
+    Ship(int , int);
+    void setShip(int);
+    int getShip();
+    void setshipMilk(int);
+    int getshipMilk();
+};
+class Cow
+{
+    int yogurt;
+    int butter;
+    int cowMilk;
+    int CowCount;
+    public:
+    Cow(int , int );
+    void setcow(int);
+    int getcow();
+    void setcowMilk(int);
+    int getcowMilk();
+    void setbutter(int);
+    int getbutter();
+    void setyogurt(int);
+    int getyogurt();
+};
+int main()
+{
+
+    return 0;
+}
