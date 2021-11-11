2406296fc02ebb5ae463573f1e4f3793bb999616
irMakaveli <hosein.acc2@gmail.com>
2021-11-11 13:16:08 -0500

    cmake created

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/ap-project-p1.cpp.o"
+  "CMakeFiles/app.dir/src/src.cpp.o"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
